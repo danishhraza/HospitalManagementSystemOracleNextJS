cfd22bf605525a03167dd60cafe454e383aee50c
Danish Raza <danishrock11@gmail.com>
2023-12-10 19:26:42 +0500

    working appointment

--- db/db.sql
+++ db/db.sql
@@ -30,7 +30,7 @@ CREATE TABLE PATIENT (
 
 
 CREATE TABLE APPOINTMENT (
-  AppointmentID NUMBER PRIMARY KEY,
+  AppointmentID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   PatientID NUMBER NOT NULL,
   DoctorID NUMBER NOT NULL,
   AppointmentDate DATE NOT NULL,
@@ -138,44 +138,12 @@ SELECT * FROM PHARMACY;
 SELECT * FROM DISCONTINUED_PROCEDURES;
 
 
--- INSERT INTO USERS (Email, Password, Role)
--- VALUES ('doctor3@example.com', 'doctorpass', 'doctor');
-
--- INSERT INTO USERS (Email, Password, Role)
--- VALUES ('patient@example.com', 'patientpass');
-
--- Insert dummy data for a doctor
--- INSERT INTO DOCTOR (FirstName, LastName, Gender, ContactNumber, Specialization, UserID)
--- VALUES ('John', 'Doe', 'Male', '55512345567', 'Cardiologist', 4);
-
--- Insert dummy data for a patient
--- INSERT INTO PATIENT (FirstName, LastName, Gender, DateOfBirth, ContactNumber, Address, UserID)
--- VALUES ('Jane', 'Smith', 'Female', TO_DATE('1985-03-22', 'YYYY-MM-DD'), '9876543210', '456 Elm St', 2);
-
--- INSERT INTO APPOINTMENT (PatientID, DoctorID, AppointmentDate, Status)
--- VALUES (41, 2, TO_DATE('2024-11-15', 'YYYY-MM-DD'), 'Pending Approval');
-
--- INSERT INTO PROCEDURE (ProcedureName, Description, Price, DoctorID)
--- VALUES ('Eye Examination', 'Comprehensive eye examination', 60, 23);
-
--- INSERT INTO PHARMACY (PharmacyName, Location, ContactNumber)
--- VALUES ('Best Meds', '789 Maple Drive', '555-9876');
-
--- DELETE FROM USERS
--- WHERE UserID = 2;
-
--- DELETE FROM PATIENT
--- WHERE UserID = 66;
-
--- DELETE FROM DOCTOR
--- WHERE UserID = 1;
-
-
 -- DROP TABLE PROCEDURE;
+-- DROP TABLE DISCONTINUED_PROCEDURES;
 -- DROP TABLE PHARMACY;
 -- DROP TABLE APPOINTMENT;
--- DROP TABLE DOCTOR;
 -- DROP TABLE PATIENT;
+-- DROP TABLE DOCTOR;
 -- DROP TABLE USERS;
 
 
